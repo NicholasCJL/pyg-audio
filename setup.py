4d8a00e3c2a82dfe1c2b308cb69dad9c9ee95e45
NicholasCJL <chong.jiale.nicholas@gmail.com>
2023-09-09 17:22:05 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygaudio',
+      version='0.2.0',
+      description='Extensible template for basic audio creation and playback'
+      ' using python with pygame.',
+      url='https://github.com/NicholasCJL/pyg-audio',
+      author='Nicholas CJL',
+      packages=['pygaudio'],
+      package_dir={'': 'src'},
+      zip_safe=False)
