f2bda7898fb34759e87a31e7eb5b3403cea2fc36
NicholasCJL <chong.jiale.nicholas@gmail.com>
2023-09-09 17:30:06 +0800

    Updated patch version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pygaudio',
-      version='0.2.0',
+      version='0.2.1',
       description='Extensible template for basic audio creation and playback'
       ' using python with pygame.',
       url='https://github.com/NicholasCJL/pyg-audio',
