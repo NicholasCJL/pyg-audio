504b98c78013bc2c5a899ce1790ba68c0f480481
Nicholas CJL <contact@nicholascjl.dev>
2023-09-10 15:36:27 +0800

    Updated minor version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pygaudio',
-      version='0.8.2',
+      version='0.9.0',
       description='Extensible template for basic audio creation and playback'
       ' using python with pygame.',
       url='https://github.com/NicholasCJL/pyg-audio',
