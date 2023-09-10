4ebfa15cfc1cd7eceb4cb674b6356d9311126063
NicholasCJL <chong.jiale.nicholas@gmail.com>
2023-09-10 13:14:02 +0800

    Added stereo option and option to selectively add audio to channels

--- src/pygaudio/pygaudio.py
+++ src/pygaudio/pygaudio.py
@@ -7,18 +7,25 @@ import pygame
 class PygAudio:
     max_amp = 32767
 
-    def __init__(self, sample_rate: Optional[int] = None,
+    def __init__(self, num_channels: Optional[int] = None,
+                 sample_rate: Optional[int] = None,
                  relative_amp: Optional[float] = None) -> None:
         """
+        :param num_channels: Mono (1) or stereo (2). Defaults to mono.
         :param sample_rate: Sample rate of audio engine. Defaults to 44100.
         :param relative_amp: Relative amplitude of final audio (0 to 1).
             Defaults to 0.5.
         """
+        if num_channels is None:
+            num_channels = 1
+        elif num_channels not in (1, 2):
+            raise ValueError(f"{num_channels=} needs to be 1 or 2.")
         if sample_rate is None:
             sample_rate = 44100
         if relative_amp is None:
             relative_amp = 0.5
 
+        self.num_channels = num_channels
         self.sample_rate = sample_rate
         self.relative_amp = relative_amp
         self.t = None
@@ -33,7 +40,9 @@ class PygAudio:
         """
         self.t = np.linspace(0, seconds, self.sample_rate * seconds,
                              dtype=np.float64)
-        self.collected_amp = np.zeros_like(self.t)
+
+        self.collected_amp = np.zeros((len(self.t), self.num_channels),
+                                      dtype=np.float64)
         self.timestep = self.t[1] - self.t[0]
         return self.timestep
 
@@ -47,7 +56,8 @@ class PygAudio:
                     amplitude: float,
                     phase: Optional[float] = None,
                     start_offset: Optional[float] = None,
-                    stop_offset: Optional[float] = None) -> None:
+                    stop_offset: Optional[float] = None,
+                    channel: Optional[str] = None) -> None:
         """
         Create audio based on a defined wavefunction and volume function.
         :param wave_func: Function that takes in time array and returns array
@@ -61,6 +71,8 @@ class PygAudio:
             wave at. Starts at 0 if None.
         :param stop_offset: (Optional) Stopping index of time array to play
             wave at. Stops at last index if None.
+        :param channel: Audio channel to create audio in.
+            ("left", "right", "both"). Defaults to "both".
         """
         if self.collected_amp is None:
             raise UnboundLocalError("Construct time first with"
@@ -88,19 +100,33 @@ class PygAudio:
         if phase is None:
             phase = 0
 
+        if channel is None:
+            channel = "both" if self.num_channels == 2 else "left"
+        if channel not in ("left", "right", "both"):
+            raise ValueError(f"{channel=} has to be 'left' or 'right' or"
+                             " 'both'")
+
         # segment of time array the wave lasts for
         wave_time = self.t[start_offset:stop_offset+1]
-        # segment of collected amplitude the wave lasts for
-        amp_segment = self.collected_amp[start_offset:stop_offset+1]
         # modulation starts relative to t=offset
         modulation = vol_func(wave_time - wave_time[0])
         # wave starts relative to t=0
         wave = wave_func(wave_time, frequency, amplitude, phase)
-        amp_segment += modulation * wave
+        # modulated wave
+        mod_audio = modulation * wave
+
+        # add audio to relevant channels
+        if channel == "left" or channel == "both":
+            amp_segment = self.collected_amp[start_offset:stop_offset+1, 0]
+            amp_segment += mod_audio
+        if channel == "right" or channel == "both":
+            amp_segment = self.collected_amp[start_offset:stop_offset+1, 1]
+            amp_segment += mod_audio
 
     def insert_audio(self, audio: np.ndarray,
                      start_offset: Optional[float] = None,
                      stop_offset: Optional[float] = None,
+                     channel: Optional[str] = None,
                      vol_func: Optional[Callable[[np.ndarray],
                                                  np.ndarray]] = None) -> None:
         """
@@ -112,6 +138,8 @@ class PygAudio:
             wave at. Starts at 0 if None.
         :param stop_offset: (Optional) Stopping index of time array to play
             wave at. Stops at start_offset + len(audio) - 1 if None.
+        :param channel: Audio channel to insert audio in.
+            ("left", "right", "both"). Defaults to "both".
         :param vol_func: (Optional) Function that takes in time array and
             returns array of modulation coefficients. No modulation if None.
         """
@@ -134,6 +162,12 @@ class PygAudio:
             raise ValueError("Stop offset has to be greater than start"
                              " offset.")
 
+        if channel is None:
+            channel = "both" if self.num_channels == 2 else "left"
+        if channel not in ("left", "right", "both"):
+            raise ValueError(f"{channel=} has to be 'left' or 'right' or"
+                             " 'both'")
+
         # force length
         audio = np.resize(audio, stop_offset - start_offset + 1)
 
@@ -142,11 +176,18 @@ class PygAudio:
 
         # segment of time array the wave lasts for
         wave_time = self.t[start_offset:stop_offset+1]
-        # segment of collected amplitude the wave lasts for
-        amp_segment = self.collected_amp[start_offset:stop_offset+1]
         # modulation starts relative to t=offset
         modulation = vol_func(wave_time - wave_time[0])
-        amp_segment += modulation * audio
+        # modulated audio
+        mod_audio = modulation * audio
+
+        # add audio to relevant channels
+        if channel == "left" or channel == "both":
+            amp_segment = self.collected_amp[start_offset:stop_offset+1, 0]
+            amp_segment += mod_audio
+        if channel == "right" or channel == "both":
+            amp_segment = self.collected_amp[start_offset:stop_offset+1, 1]
+            amp_segment += mod_audio
 
     def play_sound(self) -> None:
         amp = (self.collected_amp
@@ -154,8 +195,10 @@ class PygAudio:
                * self.max_amp
                / np.max(self.collected_amp)).astype(np.int16)
 
-        # reshape to 2 channels because mono is broken
-        amp = np.repeat(amp.reshape(amp.size, 1), 2, axis=1)
+        if self.num_channels == 1:
+            # reshape to 2 channels because mono is broken
+            amp = np.repeat(amp, 2, axis=1)
+
         sound = pygame.sndarray.make_sound(amp)
         sound.play()
         while pygame.mixer.get_busy():
