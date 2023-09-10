53c253b3b5e81fa2ddb0a57a9f66e2b9c44ad112
NicholasCJL <chong.jiale.nicholas@gmail.com>
2023-09-10 13:45:00 +0800

    Added linear_exp function and changed function signatures of envelopes

--- src/pygaudio/envelopes.py
+++ src/pygaudio/envelopes.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 
 """
@@ -5,17 +7,35 @@ Volume modulation functions, defines the envelope of the wave.
 """
 
 
-def constant_envelope(t: np.ndarray) -> np.ndarray:
+def constant_envelope(t: np.ndarray,
+                      scale: Optional[float] = None) -> np.ndarray:
     """
     No-op, returns array of ones same shape as t.
     :param t: Array of timesteps.
+    :param scale: Not used.
     """
     return np.ones_like(t)
 
 
-def sym_ramp_envelope(t: np.ndarray) -> np.ndarray:
+def sym_ramp_envelope(t: np.ndarray,
+                      scale: Optional[float] = None) -> np.ndarray:
     """
     Symmetrically ramps up from 0 to 1, then down from 1 to 0.
     :param t: Array of timesteps.
+    :param scale: Not used.
     """
     return 1 - np.abs(2 * t / t[-1] - 1)
+
+
+def linear_exp_envelope(t: np.ndarray,
+                        scale: Optional[float] = None) -> np.ndarray:
+    """
+    Quick ramp up from 0 then decay. f(t, scale) = t*exp(1-t/scale)/scale.
+    Max amplitude of 1.
+    :param t: Array of timesteps.
+    :param scale: "Sharpness" of "hump". Larger scale corresponds to faster
+        ramp up and decay. Defaults to 1.
+    """
+    if scale is None:
+        scale = 1
+    return (t / scale) * np.exp(1 - t / scale)
